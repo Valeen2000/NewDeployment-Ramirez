fd73ca4bca314e7564e2cdadc353875107167bd0
Evelyn C. Ramirez <134569360+Valeen2000@users.noreply.github.com>
2023-05-25 10:59:34 +0800

    Add files via upload

---
+++ RAMIREZ E/WindowsApplication1/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+       
+
+        If My.Settings.AppStat = "Trial Version" Then
+
+
+            If My.Settings.LoadCount < 0 Then
+                Form2.ShowDialog()
+            End If
+            Me.Text = My.Settings.AppStat & " : Count = > " &
+            My.Settings.LoadCount
+            My.Settings.LoadCount -= 1
+            My.Settings.Save()
+
+            Else
+            Me.Text = My.Settings.AppStat
+        End If
+
+
+    End Sub
+End Class
---
+++ RAMIREZ E/WindowsApplication1/Form2.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form2
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        '0DSXHB
+        If My.Settings.SerialKey = TextBox1.Text Then
+            My.Settings.AppStat = "Full Version"
+            My.Settings.Save()
+            Close()
+        Else
+            MsgBox("Invalid Serial Key")
+        End If
+    End Sub
+
+    Private Sub Form2_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        End
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
